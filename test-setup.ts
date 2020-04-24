733485effc1276715cd5ffb492244bd1aaf41cf7
zabotg <zabot.gui@gmail.com>
2020-04-24 01:08:48 -0300

    Insert linter test mocha and configurate nodemon

--- test-setup.ts
+++ test-setup.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 import chaiAsPromised from 'chai-as-promised';
-import 'reflect-metadata'; //necessary for aws-dynamodb-data-mapper
 
 dotenv.config({
   path: '.env.test',
