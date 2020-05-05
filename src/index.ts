49a5fe2d52194e8f16e14dfd38bab5d6c07576cb
Guilherme Zabot <zabot.gui@gmail.com>
2020-05-04 23:42:58 -0300

    Insert test route and httpRequests

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ import server from './server';
 server.listen(3000, () => {
   console.log(`[SERVER] Running at http://localhost:3000`);
 });
+
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,29 @@
 import express from 'express';
-const server = express();
+import bodyParser from 'body-parser';
 
-server.get('/', (_request, response) => {
-  response.send('Hello ts-node!');
+const server = express();
+server.use( bodyParser.json());
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const somadoisNumeros = (firstNumber: number, secondNumber: number): number => {
+  return firstNumber + secondNumber;
+}
+
+server.get('/', (_req, resp) => {
+  resp.send('Hello ts-node!');
+});
+
+server.post('/send-user', (req, res) => {
+  const {firstNumber, secondNumber} = req.body;
+  if(firstNumber && secondNumber) {
+    const result = somadoisNumeros(firstNumber, secondNumber)
+    res.status(200).send(`${result}`)
+  }
+  else {
+    res.status(400).send('Wrong body parameters!')
+  }
 });
 
 export default server;
