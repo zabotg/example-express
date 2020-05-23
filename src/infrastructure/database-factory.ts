f16b659f71a280da3d496bd7e764fa65bd3cff55
Guilherme Zabot <zabot.gui@gmail.com>
2020-05-23 19:30:47 -0300

    Refactor variables

--- src/infrastructure/database-factory.ts
+++ src/infrastructure/database-factory.ts
@@ -4,8 +4,8 @@ const connection = mysql.createConnection({
   host     : '127.0.0.1',
   port     : 3306,
   user     : 'root',
-  password : 'zabotAdmin!',
-  database : 'testedb'
+  password : 'password',
+  database : 'databaseName'
 });
 
 export default connection;
