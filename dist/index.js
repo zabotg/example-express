644419c74e9cda592596afcb7a20f3c7435cef7c
zabotg <zabot.gui@gmail.com>
2021-03-08 22:57:12 -0300

    Remove dist folder

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-console.log(`Starting server with STAGE "${process.env.NODE_ENV}"`);
-(() => server_1.createExpressServer())();
-//# sourceMappingURL=index.js.map
